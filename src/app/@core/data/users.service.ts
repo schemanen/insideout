036a596f34405bd954ab77baa0f8caa98bfb9c11
Kim Myhrman <kim.myhrman@gmail.com>
2018-05-05 18:17:05 +0000

    fix of picture

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -14,6 +14,7 @@ export class UserService {
     lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
     alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png' },
     kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png' },
+    kim: { name: 'Kim Myhrman', picture: 'assets/images/kimmyhrman.jpg' },
   };
 
   private userArray: any[];
--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit} from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
   
+  
   constructor() { }
 
   ngOnInit() {
